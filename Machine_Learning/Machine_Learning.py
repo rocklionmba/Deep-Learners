f1df3f43a20bae2aef5c28bb6eac7966ed83797b
emariwileslee <61813161+emariwileslee@users.noreply.github.com>
2020-04-04 18:25:40 -0400

    Update Machine_Learning.py
    Now with dynamic filename

--- Machine_Learning/Machine_Learning.py
+++ Machine_Learning/Machine_Learning.py
@@ -8,48 +8,38 @@ import os
 import matplotlib.pyplot as plt
 import numpy as np
 
-# 1. create yolo instance
-yolo_detector = create_yolo("ResNet50", ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"], 416)
-
-# 2. load pretrained weighted file
-# Pretrained weight file is at https://drive.google.com/drive/folders/1Lg3eAPC39G9GwVTCH3XzF73Eok-N-dER
-
-DEFAULT_WEIGHT_FILE = os.path.join(yolo.PROJECT_ROOT, "weights.h5")
-yolo_detector.load_weights(DEFAULT_WEIGHT_FILE)
-
-#%matplotlib inline  
-DEFAULT_IMAGE_FOLDER = os.path.join(yolo.PROJECT_ROOT, "imgs")
-#Change this file to load new images
-img_files = [os.path.join(DEFAULT_IMAGE_FOLDER, "datetime.jpg")]
-imgs = []
-for fname in img_files:
-    img = cv2.imread(fname)
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    imgs.append(img)
-    plt.imshow(img)
-    plt.show()
+def detector(image_loc):
+    # 1. create yolo instance
+    yolo_detector = create_yolo("ResNet50", ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"], 416)
     
-# 4. Predict digit region
-prob_buffer = np.empty
-THRESHOLD = 0.3
-for img in imgs:
-    boxes, probs = yolo_detector.predict(img, THRESHOLD)
-    prob_buffer = probs
-    # 4. save detection result
-    image = draw_scaled_boxes(img,
-                              boxes,
-                              probs,
-                              ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"])
+    # 2. load pretrained weighted file
+    DEFAULT_WEIGHT_FILE = os.path.join(yolo.PROJECT_ROOT, "weights.h5")
+    yolo_detector.load_weights(DEFAULT_WEIGHT_FILE)
+      
+    # 3. Load images
+    DEFAULT_IMAGE_FOLDER = os.path.join(yolo.PROJECT_ROOT, "imgs")
 
-    print("{}-boxes are detected.".format(len(boxes)))
-    plt.imshow(image)
-    plt.show()
+    img_files = [os.path.join(DEFAULT_IMAGE_FOLDER, image_loc)]
+    imgs = []
+    for fname in img_files:
+        img = cv2.imread(fname)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        imgs.append(img)
+        plt.imshow(img)
+        plt.show()
+        
+    # 4. Predict digit region
+    THRESHOLD = 0.3
+    for img in imgs:
+        boxes, probs = yolo_detector.predict(img, THRESHOLD)
+        
+    return probs
 
 def first_nonzero(arr, axis, invalid_val=-1):
     mask = arr!=0
     return np.where(mask.any(axis=axis), mask.argmax(axis=axis), invalid_val)
 
-def get_number():
+def get_number(prob_buffer):
     output_vector = first_nonzero(prob_buffer,axis=1,invalid_val=-1)
     output = ""
     
