2521732d19ffaa6af2cddd2b50b3fdf28bea3de1
con321dude <43625607+con321dude@users.noreply.github.com>
2020-04-05 18:39:13 -0400

    added math game

--- UI/MainWindow.py
+++ UI/MainWindow.py
@@ -19,6 +19,7 @@ from PyQt5.QtCore import Qt, QPoint
 import sys
 from Whiteboard import Whiteboard
 
+
 class Window(QMainWindow):
     def __init__(self):
         super().__init__()
@@ -33,31 +34,46 @@ class Window(QMainWindow):
         self.filePath = ""
         self.setWindowTitle("Whiteboard Application")
 
-        ui = uic.loadUi("UI/MainWindow.ui",self)
-        
-        #All findChild should go here
+        ui = uic.loadUi("MainWindow.ui", self)
+
+        # All findChild should go here
         self.whiteboardMainButton = self.findChild(QtWidgets.QPushButton, 'whiteboardMainButton')
+        self.mathgameMainButton = self.findChild(QtWidgets.QPushButton, 'mathgameMainButton')
         self.mainStackedWidget = self.findChild(QtWidgets.QStackedWidget, 'mainStackedWidget')
         self.backButton = self.findChild(QtWidgets.QPushButton, 'backButton')
         self.clearButton = self.findChild(QtWidgets.QPushButton, 'clearButton')
         self.mainWhiteboard = self.findChild(Whiteboard, 'mainWhiteboard')
+        self.scratchPaperWhiteboard = self.findChild(Whiteboard, 'scratchPaperWhiteboard')
+        self.answerBoxWhiteboard = self.findChild(Whiteboard, 'answerBoxWhiteboard')
         self.newWhiteboard = self.findChild(QtWidgets.QAction, 'actionWhiteboard')
         self.saveWhiteboard = self.findChild(QtWidgets.QAction, 'actionSave')
         self.saveAsWhiteboard = self.findChild(QtWidgets.QAction, 'actionSave_As')
         self.closeWhiteboard = self.findChild(QtWidgets.QAction, 'actionClose')
         self.openWhiteboard = self.findChild(QtWidgets.QAction, 'actionOpen')
-        #End findChild
-
+        #self.operatorComboBox = self.findChild(QtWidgets.QComboBox, 'operatorComboBox')
+        # End findChild
 
         self.mainWhiteboard.initalize()
         self.mainWhiteboard.setMouseTracking(True)
         self.mainWhiteboard.setFocusPolicy(Qt.StrongFocus)
-        
-        if (type(self.mainStackedWidget) == "NoneType"): print("none")
+
+        self.scratchPaperWhiteboard.initalize(691, 581)
+        self.scratchPaperWhiteboard.setMouseTracking(True)
+        self.scratchPaperWhiteboard.setFocusPolicy(Qt.StrongFocus)
+
+        self.answerBoxWhiteboard.initalize(361, 231)
+        self.answerBoxWhiteboard.setMouseTracking(True)
+        self.answerBoxWhiteboard.setFocusPolicy(Qt.StrongFocus)
+
+        if (type(self.mainStackedWidget) == "NoneType"):
+            print("none")
 
         self.whiteboardMainButton.clicked.connect(lambda: self.setNewWhiteboard())
+        self.mathgameMainButton.clicked.connect(lambda: self.setNewMathgame())
         self.backButton.clicked.connect(lambda: self.mainStackedWidget.setCurrentIndex(0))
+        self.backButton_3.clicked.connect(lambda: self.mainStackedWidget.setCurrentIndex(0))
         self.clearButton.clicked.connect(lambda: self.clear())
+        self.clearButton_3.clicked.connect(lambda: self.clear())
         self.newWhiteboard.triggered.connect(lambda: self.setNewWhiteboard())
         self.openWhiteboard.triggered.connect(lambda: self.openExistingWhiteboard())
         self.saveWhiteboard.triggered.connect(lambda: self.save())
@@ -70,7 +86,8 @@ class Window(QMainWindow):
         self.mainWhiteboard.reset()
 
     def openExistingWhiteboard(self):
-        self.filePath, _ = QFileDialog.getOpenFileName(self, "Open Whiteboard", "", "Whiteboard file (*.wtbd)")
+        self.filePath, _ = QFileDialog.getOpenFileName(
+            self, "Open Whiteboard", "", "Whiteboard file (*.wtbd)")
 
         pixmap = QPixmap()
         pixmap.load(self.filePath)
@@ -80,21 +97,25 @@ class Window(QMainWindow):
 
     def save(self):  # save file function
         if (self.filePath == ""):
-            self.filePath, _ = QFileDialog.getSaveFileName(self, "Save Whiteboard", "", "Whiteboard file (*.wtbd)")
+            self.filePath, _ = QFileDialog.getSaveFileName(
+                self, "Save Whiteboard", "", "Whiteboard file (*.wtbd)")
 
         pixmap = self.mainWhiteboard.pixmap()
         self.setWindowTitle("Whiteboard Application - " + self.filePath)
-        pixmap.save(self.filePath, "PNG" )
+        pixmap.save(self.filePath, "PNG")
 
     def saveAs(self):  # save file function
-        self.filePath, _ = QFileDialog.getSaveFileName(self, "Save Whiteboard", "", "Whiteboard file (*.wtbd)")
+        self.filePath, _ = QFileDialog.getSaveFileName(
+            self, "Save Whiteboard", "", "Whiteboard file (*.wtbd)")
 
         pixmap = self.mainWhiteboard.pixmap()
         self.setWindowTitle("Whiteboard Application - " + self.filePath)
-        pixmap.save(self.filePath, "PNG" )
+        pixmap.save(self.filePath, "PNG")
 
     def clear(self):  # clear screen function
         self.mainWhiteboard.reset()
+        self.scratchPaperWhiteboard.reset()
+        self.answerBoxWhiteboard.reset()
 
     def setCloseWhiteboard(self):
         self.mainWhiteboard.reset()
@@ -102,6 +123,20 @@ class Window(QMainWindow):
         self.setWindowTitle("Whiteboard Application")
         self.mainStackedWidget.setCurrentIndex(0)
 
+    def setNewMathgame(self):
+        self.mainStackedWidget.setCurrentIndex(2)
+        self.setWindowTitle("Whiteboard Application - Math Game")
+        self.scratchPaperWhiteboard.reset()
+        self.answerBoxWhiteboard.reset()
+
+
+"""
+        operator = str(operatorComboBox.currentText())
+        print(operator)
+
+    if(operator == "Addition"):
+        operatorText.
+"""
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
